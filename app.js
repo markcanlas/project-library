846e9712b8d23abec153a0ac96e71b79cf0948fc
markcanlas <m4rkd0tc0de@gmail.com>
2021-09-21 20:45:33 +0800

    Add style and add book function

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
+let myLibrary = [];
+
 const container = document.querySelector('.book-container');
 const addBtn = document.querySelector('.add-btn');
 const popupBtn = document.querySelector('.popup-wrapper');
 const closeBtn = document.querySelector('.close');
+const addTitle = document.querySelector('.title');
+const addAuthor = document.querySelector('.author');
+const addPages = document.querySelector('.total-pages');
+const submitBtn = document.querySelector('.submit-btn');
+const readCheck = document.querySelector('.check-box')
+const form = document.querySelector('.myForm');
 
-let myLibrary = [];
 
-addBtn.onclick = () => popupBtn.style.display = 'block';
-closeBtn.onclick = () => popupBtn.style.display = 'none';
+closeBtn.onclick = e => {
+    e.preventDefault();
+    popupBtn.style.display = 'none';
+    form.reset();
+} 
+    
+
+addBtn.onclick = () => {
+popupBtn.style.display = 'block';
+}
+    
+submitBtn.onclick = e => {
+    e.preventDefault();
+    popupBtn.style.display = 'none';
+    addBookToLibrary();
+    displayBooks();
+    form.reset();
+}
 
 
 
@@ -21,17 +44,19 @@ class Book {
 
 function addBookToLibrary() {
     // do stuff here
-    let bookTitle= prompt('Book title?');
-    let bookAuthor = prompt('Book author?');
-    let bookPages = prompt('Total book pages?');
-    let bookRead= prompt('Book have read or not?');
+    let bookTitle = addTitle.value
+    let bookAuthor = addAuthor.value
+    let bookPages = addPages.value
+    let bookRead= readCheck.value
 
     const books = new Book (bookTitle, bookAuthor, bookPages, bookRead)
     myLibrary.push(books);
 }
  
 function displayBooks() {
-    myLibrary.forEach( book => {
+
+
+    for(let i = 0; i < myLibrary.length; i++) {
         let card = document.createElement('div');
         let h1 = document.createElement('h3')
         let h2 = document.createElement('h3')
@@ -44,11 +69,17 @@ function displayBooks() {
         card.append(h2);
         card.append(h3);
         card.append(btn)
-        h1.textContent = `Title: "${book.title}"`
-        h2.textContent = `Written by: ${book.author}`
-        h3.textContent = `Total pages of: ${book.totalPages}`
-        btn.innerHTML = `Book have ${book.bookmark}`
-    })
+        h1.textContent = `Title: "${myLibrary[i].title}"`
+        h2.textContent = `Written by: ${myLibrary[i].author}`
+        h3.textContent = `Total pages of: ${myLibrary[i].totalPages}`
+        btn.innerHTML = `Book have ${myLibrary[i].bookmark}`
+    }
+
+
+    // myLibrary.forEach( book => {
+        
+    // })
 }
-displayBooks();
+
+
 
